3d7fae05fc3bba0326858694fc62a1fe66760e05
oxidetor <prateek.girdhar@outlook.com>
2022-06-24 19:37:54 -0700

    Format scripts using rubocop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
-    (arr.length-1).times do
-        swap_count = 0
-        arr.each_with_index do |elem, idx|
-            break if idx+1 == arr.length
-            if elem > arr[idx+1] 
-                temp = arr[idx]
-                arr[idx] = arr[idx+1]
-                arr[idx+1] = temp
-                swap_count += 1
-            end
-        end
-        break if swap_count == 0
+  (arr.length - 1).times do
+    swap_count = 0
+    arr.each_with_index do |elem, idx|
+      break if idx + 1 == arr.length
+
+      next unless elem > arr[idx + 1]
+
+      arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+      swap_count += 1
     end
-    arr
+    break if swap_count.zero?
+  end
+  arr
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,14 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# frozen_string_literal: true
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 words = "Howdy partner, sit down! How's it going?"
 
 def substrings(words, dictionary)
-    dictionary.reduce(Hash.new(0)) do | acc, dict_word |
-        words.split.each do |word| 
-            acc[dict_word] += 1 if word.downcase.include?(dict_word)
-        end
-        acc
+  dictionary.each_with_object(Hash.new(0)) do |dict_word, acc|
+    words.split.each do |word|
+      acc[dict_word] += 1 if word.downcase.include?(dict_word)
     end
+  end
 end
 
 p substrings(words, dictionary)
